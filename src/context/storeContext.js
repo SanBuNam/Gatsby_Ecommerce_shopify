9a1791a197e70571f59af50287853b4ca1d312c4
sanbunam <softdavidlee@gmail.com>
2019-12-25 13:22:04 -0800

    shopify buy client

--- src/context/storeContext.js
+++ src/context/storeContext.js
@@ -1,9 +1,16 @@
 import React, { createContext, useState } from 'react'
+import Client from 'shopify-buy'
+
+const client = Client.buildClient({
+    domain: "softdavidlee.myshopify.com",
+    storefrontAccessToken: "c920597ddc729c0609fc097b3918a577"
+})
 
 const defaultValues = {
     isCartOpen: false,
     cart: [],
-    addProductToCart: () => { console.log("added!") }
+    addProductToCart: () => { console.log("added!") },
+    client,
 }
 
 export const StoreContext = createContext(defaultValues)
